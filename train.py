fafba4b355acf27ececc73e98fbba946b46777c2
Ronaldo Cardozo <ronaldo.cardozo@wardenautomation.com>
2020-09-16 22:46:38 -0400

    Added testing modules

--- train.py
+++ train.py
@@ -283,7 +283,7 @@ def train(hyp, opt, device, tb_writer=None):
                 mem = '%.3gG' % (torch.cuda.memory_reserved() / 1E9 if torch.cuda.is_available() else 0)  # (GB)
                 s = ('%10s' * 2 + '%10.4g' * 3) % (
                     '%g/%g' % (epoch, epochs - 1), *mloss)
-                pbar.set_description(s)
+                #pbar.set_description(s)
 
                 # Plot
                 if ni < 3:
